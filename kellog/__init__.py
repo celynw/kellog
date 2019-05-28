14c01781c84a1c05303f67eae11442f6fa4273ad
Celyn Walters <3299161+celynw@users.noreply.github.com>
2019-05-28 16:19:29 +0100

    Added main file

---
+++ kellog/__init__.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import logging as _logging
+import colorama as _colorama
+
+_logger = None
+
+#===================================================================================================
+class _ColouredFormatter(_logging.Formatter):
+	def __init__(self, msg):
+		super().__init__(msg)
+
+	def format(self, record):
+		if (record.levelname == "DEBUG"):
+			prefix = _colorama.Fore.GREEN
+			record.levelname = "[DEBG]"
+		elif (record.levelname == "INFO"):
+			prefix = _colorama.Fore.WHITE
+			record.levelname = "[INFO]"
+		elif (record.levelname == "WARNING"):
+			prefix = _colorama.Fore.YELLOW
+			record.levelname = "[WARN]"
+		elif (record.levelname == "ERROR"):
+			prefix = _colorama.Fore.RED
+			record.levelname = "[ERR!]"
+		elif (record.levelname == "CRITICAL"):
+			prefix = _colorama.Fore.RED + _colorama.Style.BRIGHT
+			record.levelname = "[CRIT]"
+		else:
+			prefix = ""
+		suffix = _colorama.Style.RESET_ALL
+		return prefix + _logging.Formatter.format(self, record) + suffix
+
+
+#===================================================================================================
+def _setup_logger(file, name="logger", reset=False):
+	if (reset):
+		open(file, "w").close() # Delete contents
+	global _logger
+	if _logger:
+		_logger.handlers = []
+	_logger = _logging.getLogger(name)
+	_logger.setLevel(_logging.DEBUG)
+	ch = _logging.StreamHandler()
+	ch.setLevel(_logging.DEBUG)
+	formatting = "%(levelname)s %(message)s"
+	ch.setFormatter(_ColouredFormatter(formatting))
+	_logger.addHandler(ch)
+	if file:
+		fh = _logging.FileHandler(file)
+		fh.setLevel(_logging.DEBUG)
+		fh.setFormatter(_logging.Formatter(formatting))
+		_logger.addHandler(fh)
+
+
+#===================================================================================================
+def _force_to_string(*args):
+	msg = ""
+	if (len(args) > 0):
+		msg = str(args[0])
+	if (len(args) > 1):
+		for arg in args[1:]:
+			msg += f" {str(arg)}"
+	return msg
+
+
+#===================================================================================================
+def debug(*args):
+	_logger.debug(_force_to_string(*args))
+def info(*args):
+	_logger.info(_force_to_string(*args))
+def warning(*args):
+	_logger.warning(_force_to_string(*args))
+def error(*args):
+	_logger.error(_force_to_string(*args))
+def critical(*args):
+	_logger.critical(_force_to_string(*args))
+
+
+#===================================================================================================
+if (__name__ == "__main__"):
+	warning("log.py is supposed to be imported with 'from log import *'")
+else:
+	if not _logger:
+		_setup_logger("/tmp/log", "default_logger", True)
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+	name = "kellog",
+	version = "0.1",
+	description = "Easy logging",
+	author = "Celyn Walters",
+	packages = ["kellog"],
+	# install_requires=["bar", "greek"], # external packages as dependencies
+)
