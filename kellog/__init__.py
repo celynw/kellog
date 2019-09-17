fec80208feba2deb92745461a61bd24e94afe6ad
Celyn Walters <3299161+celynw@users.noreply.github.com>
2019-09-17 14:01:16 +0100

    Removed underscores as should be imported normally now

--- kellog/__init__.py
+++ kellog/__init__.py
@@ -1,49 +1,56 @@
 #!/usr/bin/env python
-import logging as _logging
-import colorama as _colorama
-import subprocess as _subprocess
-import inspect as _inspect
+import logging
+import colorama
+import subprocess
+import inspect
 from pathlib import Path
 import ujson
+from sys import stdout
 
-_logger = None
+name = "kellog"
+ready = False
 
-#===================================================================================================
+# ==================================================================================================
 def debug(*args):
-	if _logger is None:
-		_setup_logger("/tmp/log", "default_logger", True)
-	_logger.debug(_force_to_string(*args))
+	if not ready:
+		setup_logger("/tmp/log", "kellog", True)
+	logger = logging.getLogger(name)
+	logger.debug(force_to_string(*args))
 def info(*args):
-	if _logger is None:
-		_setup_logger("/tmp/log", "default_logger", True)
-	_logger.info(_force_to_string(*args))
+	if not ready:
+		setup_logger("/tmp/log", "kellog", True)
+	logger = logging.getLogger(name)
+	logger.info(force_to_string(*args))
 def warning(*args):
-	if _logger is None:
-		_setup_logger("/tmp/log", "default_logger", True)
-	_logger.warning(_force_to_string(*args))
+	if not ready:
+		setup_logger("/tmp/log", "kellog", True)
+	logger = logging.getLogger(name)
+	logger.warning(force_to_string(*args))
 def error(*args):
-	if _logger is None:
-		_setup_logger("/tmp/log", "default_logger", True)
-	_logger.error(_force_to_string(*args))
+	if not ready:
+		setup_logger("/tmp/log", "kellog", True)
+	logger = logging.getLogger(name)
+	logger.error(force_to_string(*args))
 def critical(*args):
-	if _logger is None:
-		_setup_logger("/tmp/log", "default_logger", True)
-	_logger.critical(_force_to_string(*args))
+	if not ready:
+		setup_logger("/tmp/log", "kellog", True)
+	logger = logging.getLogger(name)
+	logger.critical(force_to_string(*args))
 
 
-#===================================================================================================
-def _git_rev(log=info):
-	cwd = Path(_inspect.stack()[1][1]).parent
+# ==================================================================================================
+def git_rev(log=info):
+	cwd = Path(inspect.stack()[1][1]).parent
 	try:
-		output = str(_subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], stderr=_subprocess.STDOUT, universal_newlines=True, cwd=cwd)).strip()
-	except _subprocess.CalledProcessError as exc:
+		output = str(subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], stderr=subprocess.STDOUT, universal_newlines=True, cwd=cwd)).strip()
+	except subprocess.CalledProcessError as exc:
 		error(exc.output)
 	else:
 		log("Git revision:", output)
 
 
-#===================================================================================================
-def _force_to_string(*args):
+# ==================================================================================================
+def force_to_string(*args):
 	msg = ""
 	if (len(args) > 0):
 		msg = str(args[0])
@@ -53,56 +60,63 @@ def _force_to_string(*args):
 	return msg
 
 
-#===================================================================================================
-class _ColouredFormatter(_logging.Formatter):
+# ==================================================================================================
+class ColouredFormatter(logging.Formatter):
 	def __init__(self, msg):
 		super().__init__(msg)
 
 	def format(self, record):
 		if (record.levelname == "DEBUG"):
-			prefix = _colorama.Fore.GREEN
+			prefix = colorama.Fore.GREEN
 			record.levelname = "[DEBG]"
 		elif (record.levelname == "INFO"):
-			prefix = _colorama.Fore.WHITE
+			prefix = colorama.Fore.WHITE
 			record.levelname = "[INFO]"
 		elif (record.levelname == "WARNING"):
-			prefix = _colorama.Fore.YELLOW
+			prefix = colorama.Fore.YELLOW
 			record.levelname = "[WARN]"
 		elif (record.levelname == "ERROR"):
-			prefix = _colorama.Fore.RED
+			prefix = colorama.Fore.RED
 			record.levelname = "[ERR!]"
 		elif (record.levelname == "CRITICAL"):
-			prefix = _colorama.Fore.RED + _colorama.Style.BRIGHT
+			prefix = colorama.Fore.RED + colorama.Style.BRIGHT
 			record.levelname = "[CRIT]"
 		else:
 			prefix = ""
-		suffix = _colorama.Style.RESET_ALL
-		return prefix + _logging.Formatter.format(self, record) + suffix
+		suffix = colorama.Style.RESET_ALL
+		return prefix + super().format(record) + suffix
 
 
-#===================================================================================================
-def _setup_logger(file, name="logger", reset=False):
+# ==================================================================================================
+def setup_logger(file, newName="kellog", reset=False):
+	global name, ready
+	name = newName
+
 	if reset:
 		open(file, "w").close() # Delete contents
-	global _logger
-	if _logger:
-		_logger.handlers = []
-	_logger = _logging.getLogger(name)
-	_logger.setLevel(_logging.DEBUG)
-	ch = _logging.StreamHandler()
-	ch.setLevel(_logging.DEBUG)
+
+	logger = logging.getLogger(name)
+	if logger:
+		logger.handlers = []
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.DEBUG)
+	ch = logging.StreamHandler(stream=stdout)
+	ch.setLevel(logging.DEBUG)
 	formatting = "%(levelname)s %(message)s"
-	ch.setFormatter(_ColouredFormatter(formatting))
-	_logger.addHandler(ch)
+	ch.setFormatter(ColouredFormatter(formatting))
+	logger.addHandler(ch)
+
 	if file:
-		fh = _logging.FileHandler(file)
-		fh.setLevel(_logging.DEBUG)
-		fh.setFormatter(_logging.Formatter(formatting))
-		_logger.addHandler(fh)
+		fh = logging.FileHandler(file)
+		fh.setLevel(logging.DEBUG)
+		fh.setFormatter(logging.Formatter(formatting))
+		logger.addHandler(fh)
+
+	ready = True
 
 
-#===================================================================================================
-def _write_args(args, filename="args.json", log=info):
+# ==================================================================================================
+def write_args(args, filename="args.json", log=info):
 	if log:
 		import __main__ as main
 		log(f"Main script: {main.__file__}")
