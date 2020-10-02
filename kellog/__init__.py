c39330721b7bb904ef804ae65fcaa4eb01465b81
Celyn Walters <3299161+celynw@users.noreply.github.com>
2020-10-02 16:11:29 +0100

    Rename main file to hide from top-level import

--- kellog/__init__.py
+++ kellog/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-from .kellog import setup_logger
-from .kellog import debug, info, warning, error, critical
-from .kellog import write_args
+from ._kellog import setup_logger
+from ._kellog import debug, info, warning, error, critical
+from ._kellog import write_args
