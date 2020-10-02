f1e65300bbe564733e5bc433550fc51da9bd3075
Celyn Walters <3299161+celynw@users.noreply.github.com>
2020-10-02 15:34:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 from setuptools import setup
 
 setup(
-	name = "kellog",
-	version = "0.1",
-	description = "Easy logging",
-	author = "Celyn Walters",
-	packages = ["kellog"],
+	name="kellog",
+	version="0.1",
+	description="Easy logging",
+	author="Celyn Walters",
+	url="https://github.com/celynwalters/kellog",
+	packages=["kellog"],
 	install_requires=["colorama", "ujson"],
+	python_requires=">=3.6",
 )
