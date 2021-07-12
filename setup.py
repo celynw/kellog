fd7a88fd0bb18fc0f0072d1e23fdd8179cd46937
Celyn Walters <3299161+celynw@users.noreply.github.com>
2021-07-12 11:54:51 +0100

    Update version to `0.2.0`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name="kellog",
-	version="0.1.0",
+	version="0.2.0",
 	description="Easy logging",
 	author="Celyn Walters",
 	url="https://github.com/celynw/kellog",
