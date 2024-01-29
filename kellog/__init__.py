6eca3a5f3d3c3236ca982b6780c77371df1d5f56
Celyn Walters <3299161+celynw@users.noreply.github.com>
2024-01-29 00:31:43 +0000

    Add documentation to package

--- kellog/__init__.py
+++ kellog/__init__.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
+"""Extremely easy to use python `print()` replacement with coloured output and status icons."""
 from ._kellog import Kellog, critical, debug, error, info, log_args, warning
