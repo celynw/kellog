57b349bba084ce789954fac3835613c2716a04e6
Celyn Walters <3299161+celynw@users.noreply.github.com>
2024-01-29 00:40:10 +0000

    Remove shebang from `__init__.py`

--- kellog/__init__.py
+++ kellog/__init__.py
@@ -1,3 +1,2 @@
-#!/usr/bin/env python3
 """Extremely easy to use python `print()` replacement with coloured output and status icons."""
 from ._kellog import Kellog, critical, debug, error, info, log_args, warning
